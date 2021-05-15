c334d024f1252ed9c6629b838566bc0f40aee208
Sjoerd van der Hoorn <sjoerdvanderhoorn@hotmail.com>
2021-05-15 16:23:05 +0200

    Favicon and Recipes search

--- components/recipes.js
+++ components/recipes.js
@@ -8,18 +8,44 @@ Vue.component('recipes', {
             </div>
             <div class="row">
                 <div class="col s12 offset-m1">
-                    <ul>
-                        <li v-for="recipe in recipes">
-                            <a :href="'#/recipe/' + recipe.id">{{recipe.title}}</a>
-                        </li>
-                    </ul>
+                    <div class="input-field col s6">
+                        <input id="recipes_search" type="text" v-model="search">
+                        <label for="recipes_search">Search</label>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 offset-m1">
+                    <div class="row">
+                        <div class="col s12 m4" v-for="recipe in filteredRecipes">
+                            <div class="card teal">
+                                <div class="card-content white-text">
+                                    <span class="card-title">{{recipe.title}}</span>
+                                    <p>{{recipe.description}}</p>
+                                </div>
+                                <div class="card-action">
+                                    <a :href="'#/recipe/' + recipe.id">Edit</a>
+                                    <a href="#">Add to menu</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     `,
     props: ["recipes"],
     data: function() {
-        return {}
+        return {
+            search: ""
+        }
+    },
+    computed: {
+        filteredRecipes: function() {
+            var words = this.search.match(/\w+|\d+/g) || [];
+            var results = this.recipes.filter(recipe => words.every(word => JSON.stringify(recipe).includes(word)));
+            return results;
+        }
     },
     methods: {
 
